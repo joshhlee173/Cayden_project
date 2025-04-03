d0a63cf8822f5b5081ba1319b9f6276ff1691f03
joshhlee173 <lee.joshua173@gmail.com>
2025-04-03 18:35:51 -0400

    first commit, added main file

---
+++ main.java
@@ -0,0 +1,7 @@
+// Starter file for project
+public class main {
+    public static void main(String[] args) {
+        // Program execution starts here
+        System.out.println("Hello, World!");
+    }
+}

f989388aed70cd9aec351403a6a7b2a2bcdb3a0a
joshhlee173 <lee.joshua173@gmail.com>
2025-04-12 13:19:18 -0700

    added new line

--- main.java
+++ main.java
@@ -3,5 +3,6 @@ public class main {
     public static void main(String[] args) {
         // Program execution starts here
         System.out.println("Hello, World!");
+        System.out.println("Hi Cayden");
     }
 }

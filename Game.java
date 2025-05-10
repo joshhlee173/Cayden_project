c6bb96326190cb72278c125111db18dd64af040d
joshhlee173 <lee.joshua173@gmail.com>
2025-05-10 13:02:11 -0700

    Game object

---
+++ Game.java
@@ -0,0 +1,22 @@
+public class Game {
+    private Town town;
+    private MainCharacter player;
+    
+    public void start() {
+        System.out.println("Welcome to the Town Building Game!");
+
+        // New town
+
+        // Add NPCs
+
+        // Set up the main character
+
+        // Antagonist (future quest/obstacle)
+
+        // Game intro or menu here
+    }
+
+    private void showGoals() {
+
+    }
+}
